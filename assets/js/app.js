2dcd9f712d08beae0654dd741b84ce50f27dc9ca
mcgidoug <mcgidoug@gmail.com>
2022-04-11 20:58:35 -0500

    updated js functionality

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+// weather api function
+let weather = {
+  apiKey: "f1ca64ad1d9048ede6bc992bc906defa",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+    document.querySelector(".description") = innerText = description;
+    document.querySelector(".temp") = innerText = temp + "°C";
+  },
+};
