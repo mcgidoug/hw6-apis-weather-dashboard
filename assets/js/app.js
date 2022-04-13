5b22f3aada7f6851ad5281282e8f428ae8015bb2
mcgidoug <mcgidoug@gmail.com>
2022-04-12 22:37:49 -0500

    updated html and js functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,8 @@
-// weather api function
+// declare weather api functions within weather variable
 let weather = {
+  // api key
   apiKey: "f1ca64ad1d9048ede6bc992bc906defa",
+  // fetch weather function - fetch from open weather with city input
   fetchWeather: function (city) {
     fetch(
       "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -9,18 +11,31 @@ let weather = {
         this.apiKey
     )
       .then((response) => response.json())
-      .then((data) => this.displayWeather(data));
+      .then((data) => this.weatherFunction(data));
   },
-  displayWeather: function (data) {
-    const { name } = data;
-    const { icon, description } = data.weather[0];
-    const { temp, humidity } = data.main;
-    const { speed } = data.wind;
-    console.log(name, icon, description, temp, humidity, speed);
-    document.querySelector(".city").innerText = "Weather in " + name;
+  // sorting through fetched data to retrieve city weather info
+  weatherFunction: function (data) {
+    let { name } = data;
+    let { icon, description } = data.weather[0];
+    let { temp, humidity } = data.main;
+    let { speed } = data.wind;
+    // query selectors referencing classes on html to insert api weather data
+    document.querySelector(".cityName").innerText = "Weather in " + name;
     document.querySelector(".icon").src =
       "https://openweathermap.org/img/wn/" + icon + "@2x.png";
-    document.querySelector(".description") = innerText = description;
-    document.querySelector(".temp") = innerText = temp + "°C";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temperature").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".windSpeed").innerText =
+      "Wind Speed: " + speed + "km/h";
+  },
+  // target searchbar value - use for input value of weather api
+  searchCity: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
   },
 };
+// click search to find weather
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.searchCity();
+});
